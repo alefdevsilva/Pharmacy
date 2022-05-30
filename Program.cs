64416c35c9b9b37bd92fba1cfc56d2128e6ff186
Alef Silva <56990114+alefdevsilva@users.noreply.github.com>
2022-05-30 11:16:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+namespace Pharmacy
+{
+    class Program{
+        static void Main(string[] args){
+               string path = @"C:\Users\DELL\Documents\endereco.txt";
+               StreamReader sr = null;
+               string line = "";
+               try{
+                 sr = File.OpenText(path);
+                 while(!sr.EndOfStream){
+                       line = sr.ReadLine();
+                      Console.WriteLine(line);
+                 }
+               }
+               catch(IOException e){
+                  System.Console.WriteLine("An Error occured");
+                  System.Console.WriteLine(e.Message);
+               }
+               finally{
+                 if(sr != null) sr.Close();
+       }
+       Process firefox = new Process();
+       firefox.StartInfo.FileName = "c:\\Program Files\\Mozilla Firefox\\firefox.exe";
+       firefox.StartInfo.Arguments = "google.com/" + "search?q=" + line;
+       firefox.Start();
+    }
+ }
+}
